3fb1d6db4a664c49dcb138d711bf8b4e427b508e
Santosh Kumar <dssant@gmail.com>
2020-04-18 23:02:59 +0530

    Update chart_properties.py

--- mattplotlib/chart_properties.py
+++ mattplotlib/chart_properties.py
@@ -19,3 +19,13 @@ plt.plot(x , y)
 plt.annotate(xy=[2,1], s = "Second Entry")
 plt.annotate(xy=[4,6], s = "Third Entry")
 plt.show()
+
+## Adding legend
+plt.xlabel('Distance')
+plt.ylabel('Time')
+plt.title("Graph Drawing")
+plt.plot(x, y)
+plt.plot(x, z)
+plt.plot(x, t)
+plt.legend(['Race One','Race Two','Race Three'] , loc = 4)
+plt.show()
